a2d0bfc1116855dda89f1cf039b6a87648d56fff
Puneeth Chaganti <punchagan@muse-amuse.in>
2014-12-17 03:14:35 +0530

    Log debug and error messages.

--- instapaper.py
+++ instapaper.py
@@ -17,13 +17,19 @@ def _add_url(url, title):
     username, _, password = netrc.netrc().authenticators('instapaper')
     instapaper_url = 'https://www.instapaper.com/api/add'
 
-    requests.post(
+    response = requests.post(
         instapaper_url,
         data=dict(url=url, title=title),
         auth=(username, password)
     )
 
-    # fixme: log responses
+    if response.status_code//100 == 2:
+        LOG.debug('Added url {}'.format(url))
+
+    else:
+        LOG.error('Failed to add url {} with message {}'.format(
+            url, response.text)
+        )
 
 if __name__ == '__main__':
-    _add_url('punchagan.muse-amuse.in', 'Test page')
+    _add_url('punchagan.muse-amuse.inx', 'Test page')
