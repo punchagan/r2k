87eea66d65bca4d22b701a8143e5e321b8c742c6
Puneeth Chaganti <punchagan@muse-amuse.in>
2014-12-18 21:25:37 +0530

    Use a json file to collect all the posts.
    Can be preserved date-wise.

--- collect.py
+++ collect.py
@@ -6,40 +6,29 @@ Uses newspaper's capabilities to get the html for the article.
 
 """
 
+import json
 import logging as _logging
 from os.path import join
-from urllib.parse import quote
 
 from lxml.html import clean, fromstring, tostring
-from newspaper import Article
 
-from utils import get_template, HERE
+from utils import HERE
 
 LOG = _logging.getLogger(__name__)
 
 
 def get_article_html(feed, parsed, entry, guid, message):
-    # html = _get_article_html(entry['link'], entry['title'])
     html = _clean_js_and_styles(entry['summary'])
-    path = join(HERE, 'inbox', quote(entry['link'], safe=''))
-    template = get_template('article.html')
-    content = template.render(**{'html': html, 'entry': entry})
-    with open(path, 'w') as f:
-        f.write(content)
-
-
-def _get_article_html(url, title):
-    article = Article(url, title, keep_article_html=True)
-    article.download()
-    article.parse()
-    if article.doc is None:
-        LOG.error('Failed to parse {}'.format(url))
-        html = ''
-
-    else:
-        html = str(article.article_html, encoding='utf-8')
-
-    return html
+    path = join(HERE, 'inbox', 'digest.json')
+    data = _read_db(path)
+    data[guid] = {
+        'content': str(html, encoding='utf-8'),
+        'title': entry['title'],
+        'url': entry['link'],
+        'author': entry['author'],
+        'updated': entry['updated'],
+    }
+    _write_db(path, data)
 
 
 def _clean_js_and_styles(html):
@@ -47,9 +36,17 @@ def _clean_js_and_styles(html):
     return tostring(cleaner.clean_html(fromstring(html)))
 
 
+def _read_db(path):
+    try:
+        with open(path) as f:
+            return json.load(f)
+    except Exception:
+        return {}
+
+
+def _write_db(path, data):
+    with open(path, 'w') as f:
+        json.dump(data, f)
+
 if __name__ == '__main__':
-    content = _get_article_html(
-        'http://julien.danjou.info/blog/2014/python-distributed-membership-lock-with-tooz',
-        'Distributed group management and locking in Python with tooz '
-    )
-    print(content)
+    pass
